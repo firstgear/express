fd250a5c6c10e782b8d4bf9e88765a6dddc45b13
jmmeulem <jmmeulem@amazon.com>
2019-11-15 12:09:01 +0100

    test in Amsterdam

--- app.ts
+++ app.ts
@@ -3,7 +3,7 @@ import express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('<p>Hello, CDK! Version 5 of the API.');
+    res.send('<p>Hello, CDK! Version Amstedam of the API.');
 });
 
 app.listen(3000, err => {
